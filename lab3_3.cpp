1366ebce6bee0c448f3b64ff2e69a361759dafa6
Bsatayu <satayu_s@cmu.ac.th>
2019-12-27 22:25:51 +0700

    lab 3 อันแรก

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -2,12 +2,15 @@
 
 using namespace std; 
 
-int main (){
-     cout<< "Hi"
-    
-
-
-    return 0;
+int main()
+{  
+    float i = 0.1, ans = 0;
+while(i <= 0.69 ){
+ans = ans+ i ;
+i = i+0.01 ;
 }
-
+cout << "ans = " << ans;
+    
+return 0;
+}
 
