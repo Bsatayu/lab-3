171288e51912c11af6386fd3ea019719db1285f2
Bsatayu <satayu_s@cmu.ac.th>
2020-01-01 12:58:51 +0700

    lab 3 อันที่สอง

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {  
     float i = 1.0 , ans = 0;
-while(i > 1.0/70.0 ){
+while(i > 1.0/20.0 ){
 ans = ans+ i ;
-i = i/(i+1) ;
+i = i/(i+5) ;
 }
 cout << "ans = " << ans;
     
